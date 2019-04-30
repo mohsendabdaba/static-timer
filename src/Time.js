62053eca740a8b8d532d19ff6ba66d35b4195a0a
mohsendabdaba <dabdabmohsen@gmail.com>
2019-04-30 17:28:26 +0200

    Commit message

---
+++ src/Time.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+
+
+
+const Time = () =>{
+    return(
+
+        <div className="Time">
+        
+        <div className="first">
+        <h1>00</h1>
+        <h6>Hour </h6>
+        </div>
+
+        <div><h1>:</h1></div>
+
+        <div className="second">
+        <h1>30</h1>
+        <h6>Minute </h6>
+        </div>
+
+        <div><h1>:</h1></div>
+
+        <div className="third">
+        <h1>00</h1>
+        <h6>Second </h6>
+        </div>
+        
+      
+
+           
+    
+        </div>
+       
+    
+    
+    
+    )
+
+}
+
+
+
+export default Time
